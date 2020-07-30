3cb96f4e2504ebd6893d1921b59a319f53ca1d9f
zombieFox <crudetoaster@gmail.com>
2020-07-30 22:54:24 +0100

    Updates

--- js/initial-background.js
+++ js/initial-background.js
@@ -1,16 +1,17 @@
 // inital check for systems which do not have light or dark mode
 var nightTabData = JSON.parse(localStorage.getItem("nightTab"));
 if (!document.querySelector("html").classList.contains("is-ready")) {
+  console.log("hit");
   if (window.matchMedia("(prefers-color-scheme:light)").matches) {
     if (nightTabData) {
       if (nightTabData.state.theme.style == "dark") {
-        document.querySelector("html").setAttribute("style", "background-color:rgb(0,0,0)");
+        document.querySelector("html").setAttribute("style", "background-color: rgb(0, 0, 0)");
       };
     };
   } else if (window.matchMedia("(prefers-color-scheme:dark)").matches) {
     if (nightTabData) {
       if (nightTabData.state.theme.style == "light") {
-        document.querySelector("html").setAttribute("style", "background-color:rgb(255,255,255)");
+        document.querySelector("html").setAttribute("style", "background-color: rgb(255, 255, 255)");
       };
     };
   };

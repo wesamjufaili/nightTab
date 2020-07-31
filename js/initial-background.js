cbab00f69d4339c74de98952362d9445aa84c2bf
zombieFox <crudetoaster@gmail.com>
2020-07-31 09:06:42 +0100

    Updates

--- js/initial-background.js
+++ js/initial-background.js
@@ -1,15 +1,17 @@
 // inital check for systems which do not have light or dark mode
 var nightTabData = JSON.parse(localStorage.getItem("nightTab"));
-if (window.matchMedia("(prefers-color-scheme:light)").matches) {
-  if (nightTabData) {
-    if (nightTabData.state.theme.style == "dark") {
-      document.querySelector("html").setAttribute("style", "background-color:rgb(0,0,0)");
+if (!document.querySelector("html").classList.contains("is-ready")) {
+  if (window.matchMedia("(prefers-color-scheme:light)").matches) {
+    if (nightTabData) {
+      if (nightTabData.state.theme.style == "dark") {
+        document.querySelector("html").setAttribute("style", "background-color: rgb(0, 0, 0)");
+      };
     };
-  };
-} else if (window.matchMedia("(prefers-color-scheme:dark)").matches) {
-  if (nightTabData) {
-    if (nightTabData.state.theme.style == "light") {
-      document.querySelector("html").setAttribute("style", "background-color:rgb(255,255,255)");
+  } else if (window.matchMedia("(prefers-color-scheme:dark)").matches) {
+    if (nightTabData) {
+      if (nightTabData.state.theme.style == "light") {
+        document.querySelector("html").setAttribute("style", "background-color: rgb(255, 255, 255)");
+      };
     };
   };
 };

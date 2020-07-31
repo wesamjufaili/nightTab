7986f049fc2b7b5c302d9a30855d9b6071452341
zombieFox <crudetoaster@gmail.com>
2020-07-31 21:48:42 +0100

    Updates

--- js/initial-background.js
+++ js/initial-background.js
@@ -1,11 +1,9 @@
 // inital check for systems which do not have light or dark mode
 if (localStorage.getItem("nightTabStyle")) {
-  var themeStyle = localStorage.getItem("nightTabStyle");
   var style = document.createElement("style");
   style.type = "text/css";
   style.media = "screen";
-  console.log(style.media);
-  switch (themeStyle) {
+  switch (localStorage.getItem("nightTabStyle")) {
     case "light":
       style.innerHTML = "html, body {background-color: rgb(255, 255, 255);}";
       break;

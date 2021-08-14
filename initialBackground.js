e5848d2b8bdabeafdebc6dcba3691b636fe49aeb
zombieFox <crudetoaster@gmail.com>
2021-08-14 19:42:27 +0000

    Deploying to gh-pages from @ zombieFox/nightTab@2319aeae600bd852457cb87563164cbd3c92d0e2 🚀

--- initialBackground.js
+++ initialBackground.js
@@ -1,15 +1,1 @@
-// inital check for systems which do not have light or dark mode
-if (localStorage.getItem('nightTabStyle')) {
-  const style = document.createElement('style');
-  style.type = 'text/css';
-  style.media = 'screen';
-  switch (localStorage.getItem('nightTabStyle')) {
-    case 'light':
-      style.innerHTML = 'html, body {background-color: rgb(255, 255, 255);}';
-      break;
-    case 'dark':
-      style.innerHTML = 'html, body {background-color: rgb(0, 0, 0);}';
-      break;
-  };
-  document.querySelector('head').appendChild(style);
-};
+if(localStorage.getItem("nightTabStyle")){const e=document.createElement("style");switch(e.type="text/css",e.media="screen",localStorage.getItem("nightTabStyle")){case"light":e.innerHTML="html, body {background-color: rgb(255, 255, 255);}";break;case"dark":e.innerHTML="html, body {background-color: rgb(0, 0, 0);}"}document.querySelector("head").appendChild(e)}
